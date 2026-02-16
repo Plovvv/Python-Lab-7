5149fa83a4d3ae942f915083adfb5afbc8d525b4
Fedor <fedor.radzhapov34505@gmail.com>
2026-02-16 10:22:14 +0400

    answer

---
+++ answer.py
@@ -0,0 +1,49 @@
+import requests
+import json
+
+#1 OpenWeatherMap
+city_name = 'Kirov'
+KEY = "919aa7df6fe145b188098a1b53bf315c"
+
+def getCoordinates():
+    try:
+        response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},&limit=1&appid={KEY}')
+        result = json.loads(response.text.strip('[').strip(']'))
+        lat = result['lat']
+        lon = result['lon']
+        getWeather(lat, lon)
+    except:
+        print('Город с таким названием не был найден.')
+
+
+
+def getWeather(lat, lon):
+    response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={KEY}&units=metric&lang=ru')
+    result = json.loads(response.text)
+    weather = result['weather'][0]['description']
+    temp = result['main']['temp']
+    humidity = result['main']['humidity']
+    pressure = result['main']['pressure']
+    print(f'Погода в городе {result['name']}: {weather}\nтемпература: {temp}°C\nвлажность: {humidity}%\nдавление: {pressure}hPa')
+
+
+
+if __name__ == '__main__':
+    getCoordinates()
+
+
+
+#2 - News API
+KEY = '96dad5ad89854f498081c23710dde0ef'
+
+def getNews(theme):
+    response = requests.get(f'https://newsapi.org/v2/everything?q={theme}&language=ru&pageSize=1&apiKey={KEY}')
+    result = json.loads(response.text)
+    source = result['articles'][0]['source']['name']
+    author = result['articles'][0]['author']
+    title = result['articles'][0]['title']
+    data = result['articles'][0]['publishedAt']
+
+    print(f'Самая новая статья на тему "{theme}": "{title}", от {author}, с {source}, от {data}')
+
+getNews('lada')
